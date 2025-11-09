f99273dfd0eadd0a4477e01806234c309b058d7f
ahmedh312 <hishamahmed312@gmail.com>
2025-11-09 10:26:36 -0600

    Update page.tsx
    Add a new homepage that matches the theme

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,5 @@
 import Image from "next/image";
-import logo2 from "../public/logo2.png";
-import background from "../public/background.png";
+import background2 from "../public/background2.png";
 import Link from "next/link";
 
 export default function HomePage() {
@@ -8,33 +7,27 @@ export default function HomePage() {
     <main className="relative flex flex-col items-center justify-center h-screen w-full overflow-hidden">
       {/* Background */}
       <Image
-        src={background}
-        alt="background"
+        src={background2}
+        alt="background2"
         fill
         className="object-cover -z-10"
       />
 
-      {/* Logo */}
-      <div>
-        <Image src={logo2} alt="Spellbook Logo" width={500} height={382} />
-      </div>
-
-      {/* Subtitle */}
-      <p className="mt-2 text-center px-4 font-['Charm',_cursive] text-4xl text-white">
-        Embark on your magical quest to master savings and unlock financial
-        spells.
-      </p>
 
       {/* Button */}
       <Link href="game">
         <button
-          className="mt-10 rounded-full shadow-lg transform transition-all hover:scale-105 
-                   font-['Quintessential',_cursive] 
-                   bg-[#7c3aed] 
-                   text-2xl 
-                   py-4 
-                   px-6 
-                   text-white"
+          className=""rounded-full shadow-lg transform transition-all hover:scale-105 
+             font-['Quintessential',_cursive] 
+             bg-transparent 
+             text-[#7c3aed] 
+             border-2 border-[#7c3aed]
+             text-2xl 
+             py-4 
+             px-6 
+             hover:bg-[#7c3aed]/10
+             hover:text-[#6d28d9]
+             text-white"
         >
           BEGIN YOUR QUEST
         </button>
